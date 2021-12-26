8c1d3857c3d8d60a7047828cde0d89ad73130ae6
Elena <for-alisia@yandex.ru>
2021-12-26 12:44:26 +0100

    Fixed bug with not updating recipes

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,17 +1,17 @@
 import { RecipesService } from './recipes.service';
 import { IRecipe } from './recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage {
   recipes: IRecipe[];
   constructor(private recipesService: RecipesService) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
     this.recipes = this.recipesService.getAllRecipes();
   }
 }
